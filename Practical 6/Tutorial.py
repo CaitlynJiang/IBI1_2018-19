44c5da473c0af1b5ff99df66d923702b5e92dcec
Caitlyn Jiang <anlan.182intl.zju.edu.cn>
2019-04-03 10:03:39 +0800

    Finish permutations

---
+++ Practical 6/Tutorial.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 10:10:10 2019
+
+@author: caitlynjiang
+"""
+
+# because . matches any character, so match . with \.
+# + keep repeat until we hit a period, + apply to everything in[]square bracket
+# standard: with open...(file) needd to be modified
+# subsitution
+# Group?
+# import.re(or something else), whlie having error, the hint will show like lines200+ error, (these lines are pull from the server)
+
+
+#Below this is the modified code version 2: 
+#1. Add re.split, use list
+#2. Use re.compile
+#3.
+
+
