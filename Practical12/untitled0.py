1a78d2f2f6b09b9b743211e8388f6513f7fe080c
Caitlyn Jiang <anlan.182intl.zju.edu.cn>
2019-05-08 11:37:50 +0800

    Task 1 completed
    and plot saved

--- Practical12/untitled0.py
+++ Practical12/untitled0.py
@@ -7,7 +7,7 @@ Created on Wed May  8 09:09:31 2019
 """
 
 import numpy as np
-import matplotlib . pyplot as plt
+import matplotlib.pyplot as plt
 
 # define basic variables
 s = 9999
@@ -16,17 +16,18 @@ r = 0
 n = 10000
 b = 0.3
 y = 0.05
-pofi = i/n
-b2 = pofi*b
-b1 = 1-pofi*b
+
 sarr = np.array ([s])
 iarr = np.array ([i])
 rarr = np.array ([r])
 
 
 # 1000 loop times
-count=0
+count=1
 while count < 1000:
+    pofi = i/n # Helped by Zhang Yiwei,at first I put this line before the loop and get a very flat wierd plot
+    b2 = pofi*b
+    b1 = 1-pofi*b
     niarr = np.random.choice(range(2),s,p=[b1,b2])
     nrarr = np.random.choice(range(2),i,p=[0.95,0.05])
     ni = np.sum(niarr==1)
@@ -40,9 +41,21 @@ while count < 1000:
     iarr = np.append(iarr,i)
     rarr = np.append(rarr,r)
     count = count+1
-'''    
-    print(sarr)
-    print(iarr)
-    print(rarr)
-'''    
-plt.plot(i)
+
+#plot(), xlabel(), ylabel(1), title(), legend()
+
+plt.figure(figsize =(6,4),dpi=150)
+plt.plot(sarr,label='susceptible')
+plt.plot(iarr,label='infected')
+plt.plot(rarr,label='recovered')
+plt.xlabel("times") 
+plt.ylabel("numbers of people")
+plt.title("Infection evolution")
+plt.legend()
+plt.savefig ('Plot',type='png')
+
+
+
+
+
+
