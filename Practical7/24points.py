b79e8e54f11efd8ef95a1460a93ddbbf9c680e5c
Caitlyn Jiang <anlan.182intl.zju.edu.cn>
2019-05-14 23:53:49 +0800

    Practical 7 modified according to tutorial
    Deleted drafts
    
    P6 needed to be checked (do I need to add more pseudo codes?)

---
+++ Practical7/24points.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 09:24:35 2019
+
+@author: caitlynjiang
+"""
+
+from fractions import Fraction
+
+
+#use function to add tomrecursion times
+def dfs(n):
+    global count
+    global l
+    count=count+1
+    
+    if n==1:
+        if (float(l[0])==24):
+            return 1
+        else:
+            return 0
+        
+    for i in range (0,n):
+        for j in range (i+1,n):
+            a=l[i]
+            b=l[j]
+            l[j]=l[i-1]
+                
+            l[i]=a+b
+            if(dfs(n-1)):
+                return 1                
+                
+            l[i]=a-b
+            if(dfs(n-1)):
+                return 1
+                
+            l[i]=b-a
+            if(dfs(n-1)):
+                return 1
+                
+            l[i]=a*b
+            if(dfs(n-1)):
+                return 1
+                
+            if a:
+                l[i]= Fraction(b,a)
+                if(dfs(n-1)):
+                    return 1
+                
+            if b:
+                l[i]= Fraction(a,b)
+                if(dfs(n-1)):
+                    return 1
+            l[i]=a
+            l[j]=b
+    return 0
+                    
+print('Please input N numbers in the range of 1 to 23\n(use commas to separate):')
+num=input()
+# whether input format is correct
+l=num.split(',')
+a=False
+for i in l:
+    if i.isdigit()==True:     
+        #print(i)
+        if not 1<=int(i)<=23:
+            a=False
+            break
+        else:
+            a=True
+    else:
+        a=False
+        break
+        
+if a==False:
+    print('The input number must be intergers from 1 to 23')
+else:
+    print("Input numbers:" + num)
+    l=list(map(int,l))
+    #n=len(l)
+    count=0
+    if a==True:    
+        if (dfs(len(l))): # returns 0
+            print('Yes')
+        else: #returns 1
+            print('No')
+        print ('recurtion times:',count)
+                
+            
+        
+
