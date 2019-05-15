0ca44a015699a29a6ceaa63730e06f3c6ba2ac7d
Caitlyn Jiang <anlan.182intl.zju.edu.cn>
2019-05-15 21:23:31 +0800

    Check the instruction of P9 and added percentage identity

---
+++ Practical9/Non-gapped Global Alignment.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 10:18:06 2019
+
+@author: caitlynjiang
+"""
+
+def readmatrix(filename):                                                       # define function to read matrix so that it can be used to read different kinds of BLOSUM file
+  handle = open(filename, "r")
+  content = handle.readlines()
+  handle.close()
+
+  matrix = {}
+  letters = []
+
+  first = True
+  for line in content:
+    splitted = line.split()
+    if first:
+      for a in splitted:
+        matrix[a] = {}
+        letters.append(a)
+      first = False
+    else:
+      a = splitted[0]
+      for i in range(1, len(splitted)):
+        b = letters[i-1]
+        matrix[a][b] = splitted[i]
+
+  return matrix
+
+def ipercent(a,b):                                                              # define function to calculate identical percentage
+    count=0
+    total=len(a)
+    x=0
+    for i in a:
+        if a[x]==b[x]:
+            count +=1
+            x+=1
+        else:
+            count=count
+            x+=1
+    iscore=str('%.2f' % ((float(count)/total)*100))+'%'                         # Reserve the percentage of two decimal places
+    return iscore
+    
+matrix=readmatrix("BLOSUM62.txt")                                               # file found in a gov website, relatively trustable
+#print(matrix)
+
+seq01 = input("Please put in sequence1:" )                                      # here copy and paste the sequence
+name1 = input("Please write down the name of sequence1:")
+seq02 = input("Please put in sequence2:" )
+name2 = input("Please write down the name of sequence2:")
+seq1 = seq01.replace(' ', '')                                                   # delete the blank space in the sequence (probably \n is necessary)
+seq2 = seq02.replace(' ', '')
+score = 0
+for i in range (len(seq1)):
+    score += int(matrix[seq1[i]][seq2[i]])     
+
+pis = ipercent(seq1,seq2)                                    
+
+print("The score of",name1,"and",name2,"is",score)
+print("The percentage identity is",pis)
+
+
