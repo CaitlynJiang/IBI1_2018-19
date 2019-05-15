cc7e8a197bf0da54dd4c098b2772a6eb069ef93a
Caitlyn Jiang <anlan.182intl.zju.edu.cn>
2019-05-15 10:19:35 +0800

    Add practical 13's code and cps
    changed the repository name

--- Practical13/xml_to_cps.py
+++ Practical13/xml_to_cps.py
@@ -11,7 +11,7 @@ def xml_to_cps():
     import xml.dom.minidom
     
     # first, convert xml to cps 
-    os.system("/usr/bin/COPASI-4.16.104-Linux-64bit/bin/CopasiSE -i predator-prey.xml -s predator-prey.cps")
+    os.system("/Applications/COPASI/CopasiSE -i predator-prey.xml -s predator-prey.cps")
     
     # now comes the painful part. Just copy and paste this ok
     
