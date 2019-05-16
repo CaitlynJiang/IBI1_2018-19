2723ece6ef991ed3eae3bf610a3d3b34014cf228
Caitlyn Jiang <anlan.182intl.zju.edu.cn>
2019-05-16 11:22:19 +0800

    Added relatively neat pseudo codes for week 12 spatial_SIR
    try running it

--- Practical 12/spatial_SIR.py
+++ Practical 12/spatial_SIR.py
@@ -10,17 +10,17 @@ import numpy as np
 import matplotlib . pyplot as plt
 
 population = np.zeros((100, 100))
-outbreak = np.random.choice(range(100),2) 
-population[outbreak[0],outbreak[1]] = 1 # Random choice 0 an 1? (first 2?= 2 random number)
+outbreak = np.random.choice(range(100),2)                                       # pick 2 random number in the range(100) to become x, y
+population[outbreak[0],outbreak[1]] = 1                                         # Coordination: x,y = Random choice 0 an 1
 
-def drawmap(i, population):
-    if i in [1, 10, 50, 100]:
-        plt.figure(figsize=(6,4), dpi=150)
+def drawmap(i, population):                                                     # define function to draw hotmap
+    if i in [1, 10, 50, 100]:                                                   # show (later save) the plot at time 1,10,50,100 (as given as sample plots)
+        plt.figure(figsize=(6,4), dpi=150)                         
         plt.imshow(population, cmap='viridis', interpolation='nearest')
-        plt.savefig ('Plot_spatial'+str(i),type='png')
+        plt.savefig ('Plot_spatial'+str(i),type='png')                          # save the plot every time
     return
 
-b=0.3
+b=0.3                                                                           # initialize parameters: beta and gamma
 g=0.05
 
 '''
@@ -49,27 +49,19 @@ Btw, where is this(below) infection_snippet come from?
 '''
 
 for i in range(0,101):
-    drawmap(i,population)
-    # find infected points
+    drawmap(i,population)                                                       # find infected points
     infectedIndex = np.where(population==1)
-# loop through all infected points
     for i in range(0,len(infectedIndex[0])):
-        
-    # get x, y coordinates for each point
-        x = infectedIndex[0][i]
+        x = infectedIndex[0][i]                                                 # get x, y coordinates for each point
         y = infectedIndex[1][i]
-    # infect each neighbour with probability beta
-    # infect all 8 neighbours (this is a bit finicky, is there a better way?): 
-        for xNeighbour in range(x-1,x+2):
-            for yNeighbour in range(y-1,y+2):
-            # don't infect yourself! (Is this strictly necessary?)Not necessary, because below it writes if ...=0
-                if (xNeighbour,yNeighbour) != (x,y):
-                # make sure I don't fall off an edge
+        for xNeighbour in range(x-1,x+2):                                      
+            for yNeighbour in range(y-1,y+2):                                   # try to infect each neighbour with probability beta (infect all 8 neighbours)
+            # don't infect yourself! (Is this strictly necessary?)              # Not necessary, because below it writes if ...=0
+                if (xNeighbour,yNeighbour) != (x,y):                            # don't fall off an edge ⬇️
                      if xNeighbour != -1 and yNeighbour != -1 and xNeighbour!=100 and yNeighbour!=100:
-                    # only infect neighbours that are not already infected!
-                        if population[xNeighbour,yNeighbour]==0:
+                        if population[xNeighbour,yNeighbour]==0:                # only infect neighbours that are not already infected
                             population[xNeighbour,yNeighbour]=np.random.choice(range(2),1,p=[1-b,b])[0]
-        r = np.random.choice(range(2),1,p = [1-g,g])
+        r = np.random.choice(range(2),1,p = [1-g,g])                            # try to recover each neighbour with probability gamma, 1 for recover, 0 for not
         if r == 1:
            population[x,y]=0.5
     
